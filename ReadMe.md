5236bba01d69116a2f61b51783cd66694b04996e
prashantoswal <prashantnoswal@gmail.com>
2020-07-09 18:40:36 +0530

    Updated everything:

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,6 @@
 #Start Web Application
 
+This is web application on how to use Git and Gib HUB
 ## Introduction
 
 ## Purpose
--- src/main/java/com/xoriant/JavaUtil/App.java
+++ src/main/java/com/xoriant/JavaUtil/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World adsfadfsafdsdfsdsa" );
+        System.out.println( "Hello World adsfadfsafdsdfsdsa1" );
 
         System.out.println( "Hello World asdlasdfjl!" );
 
