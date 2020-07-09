7820481ed6f2568b1e232ac1e452bb469b5a36d1
prashantoswal <prashantnoswal@gmail.com>
2020-07-09 10:56:22 +0530

    Changed HelloWorld statement

--- src/main/java/com/xoriant/JavaUtil/App.java
+++ src/main/java/com/xoriant/JavaUtil/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World asdlasdfjl!" );
         System.out.println("Merge");
     }
     public void sayHello1() {
